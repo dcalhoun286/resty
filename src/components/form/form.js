47c406fe48dfdbd4e07225cade45c9e69f36355a
Dar-Ci Calhoun <darci.calhoun@gmail.com>
2021-05-13 11:43:04 -0700

    added needed form elements

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -5,7 +5,16 @@ class Form extends React.Component {
   render() {
     return (
       <form>
-        placeholder text
+        <input type="text" placeholder="Type your API URL here"></input>
+        <input type="radio" id="get" name="req" value="GET"></input>
+        <label for="get">GET</label>
+        <input type="radio" id="post" name="req" value="POST"></input>
+        <label for="post">POST</label>
+        <input type="radio" id="put" name="req" value="PUT"></input>
+        <label for="put">PUT</label>
+        <input type="radio" id="delete" name="req" value="DELETE"></input>
+        <label for="delete">DELETE</label>
+        <input type="submit" value="GO!"></input>
       </form>
     )
   }
