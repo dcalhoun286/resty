c2dee3d297e6387dc7920c7c44de29befe7fb273
Dar-Ci Calhoun <darci.calhoun@gmail.com>
2021-05-13 00:46:15 -0700

    prelim code for Form class

---
+++ src/components/form/form.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './form.scss';
+
+class Form extends React.Component {
+  render() {
+    return (
+      <form>
+        placeholder text
+      </form>
+    )
+  }
+}
+
+export default Form;
