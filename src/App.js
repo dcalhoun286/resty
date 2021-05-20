d2c22fcfb5525d34671267060a52360dc432b816
Dar-Ci Calhoun <darci.calhoun@gmail.com>
2021-05-20 11:39:06 -0700

    moved axios request to App component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,45 @@
 import React from 'react';
 import './app-reset.scss';
+import axios from 'axios';
+
 import Header from './components/header/header.js';
 import Form from './components/form/form.js';
+import Results from './components/results/results.js';
 import Footer from './components/footer/footer.js';
 
 class App extends React.Component {
 
+  constructor(props) {
+
+    super(props);
+
+    this.state = {
+      results: {}
+    }
+
+    this.fetchData = this.fetchData.bind(this);
+  }
+
+  async fetchData (formOptions) {
+
+    const response = await axios({
+      method: formOptions.req || 'get',
+      url: formOptions.url,
+    });
+
+    console.log('FORM OPTIONS', formOptions);
+    console.log('AXIOS RESPONSE', response);
+
+    this.setState({...this.state, results: response});
+  }
+
   render() {
+    console.log('state of results', this.state);
     return (
       <>
         <Header />
-        <Form />
+        <Form handler={this.fetchData} />
+        <Results display={this.state.results}/>
         <Footer />
       </>
     );
