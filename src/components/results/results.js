95e7b5be30a5ca0a1175490299cf3bfa6987aea8
Dar-Ci Calhoun <darci.calhoun@gmail.com>
2021-05-20 11:37:47 -0700

    moved rendered results to its own component

--- src/components/results/results.js
+++ src/components/results/results.js
@@ -1,3 +1,24 @@
 import React from 'react';
 import JSONPretty from 'react-json-pretty';
-import Form from '../'
+
+class Results extends React.Component {
+
+  render() {
+
+    return (
+      <>
+        <ul id="display">
+          <li>
+            <JSONPretty id="json-pretty" data={this.props.display.results}></JSONPretty>
+          </li>
+          <li>
+            <JSONPretty id="json-pretty" data={this.props.display.results}></JSONPretty>
+          </li>
+        </ul>
+      </>
+    )
+
+  }
+}
+
+export default Results;
